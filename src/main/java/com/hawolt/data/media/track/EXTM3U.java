fd2080d61c00e6fec2d2848eb02236bd31b2609b
hawolt <45050975+hawolt@users.noreply.github.com>
2025-02-24 09:49:23 +0100

    fix new aac format, still needs moov atom removal for mp3 to be seekable

--- src/main/java/com/hawolt/data/media/track/EXTM3U.java
+++ src/main/java/com/hawolt/data/media/track/EXTM3U.java
@@ -2,6 +2,7 @@ package com.hawolt.data.media.track;
 
 import com.hawolt.data.media.MediaLoader;
 import com.hawolt.ionhttp.request.IonResponse;
+import com.hawolt.logger.Logger;
 
 import java.nio.charset.StandardCharsets;
 import java.util.HashMap;
@@ -26,10 +27,17 @@ public class EXTM3U {
                 list.add(line);
             }
         }
+        // TODO fix moov atom for mp3 seeking after concatenation
+        Map<String, String> tags = getEXTM3UTags();
+        if (!tags.containsKey("EXT-X-MAP")) return;
+        String map = tags.get("EXT-X-MAP");
+        if (!map.matches("URI=\"(.*?)\"")) return;
+        String uri = map.substring(5, map.length() - 1);
+        list.add(0, uri);
     }
 
     private void parse(String line) {
-        String[] data = line.substring(1).split(":");
+        String[] data = line.substring(1).split(":", 2);
         if (data.length != 2 || data[0].equalsIgnoreCase("EXTINF")) return;
         map.put(data[0], data[1]);
     }
